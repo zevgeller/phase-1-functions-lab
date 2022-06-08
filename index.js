183ab911f80ba57725b1dff89669e450f556d544
Zev <zevgeller@gmail.com>
2022-06-08 10:57:11 -0500

    commit tree 76d0165f843b4e712e6d8c3776352f2deb9a6e3f

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+
+let location;
+function distanceFromHqInBlocks(location) {
+    if (location < 42)  {
+        return 42 - location;
+    }
+    return location - 42;
+    }
+console.log(distanceFromHqInBlocks(location))
+
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264;
+    
+}
+
+console.log(distanceFromHqInFeet(location))
+
+function distanceTravelledInFeet(start, destination) {
+    let totalDistance = (destination - start) * 264;
+    if (totalDistance < 0) {
+        return (totalDistance) * -1;
+    }
+    return totalDistance;
+}
+
+function calculatesFarePrice(start, destination) {
+    let fareTravelled = distanceTravelledInFeet(start, destination)
+    
+    if (fareTravelled < 400) {
+        return 0;
+    }   else{
+        if (fareTravelled > 2500) {
+            return `cannot travel that far`;
+        }
+    }  
+    if (fareTravelled > 2000) {
+        return 25
+    }
+    while (fareTravelled < 2000) {
+        let fare = (fareTravelled - 400) * 0.02;
+        return fare;
+    }
+}
